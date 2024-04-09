5e42e939523e95e9d3e32ae7a6672a43f17e57a0
Shishir <shishir3324kumar@gmail.com>
2024-04-09 22:40:00 +0530

    Basic server setup is ready!

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import express, { Express, Request, Response } from "express";
+import mongoose from "mongoose";
+import 'dotenv/config'
+
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+mongoose
+  .connect(process.env.MONGODB_URI as string)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`[server]: Server is running at http://localhost:${port}`);
+    });
+  })
+  .catch((error) => console.log(error));
